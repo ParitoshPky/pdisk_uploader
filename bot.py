f613972fd6e6ba1b1bc0fbeb5abbe354dcb0351b
Paritosh Kumar <33643362+ParitoshPky@users.noreply.github.com>
2021-09-28 16:57:14 +0530

    removed telegra.ph

--- bot.py
+++ bot.py
@@ -136,7 +136,7 @@ async def new_pdisk_url(urls):
 
 async def remove_username(new_List):
     for i in new_List:
-        if('@' in i or 't.me' in i or 'https://bit.ly/3m4gabB' in i or 'https://bit.ly/pdisk_tuts' in i):
+        if('@' in i or 't.me' in i or 'https://bit.ly/3m4gabB' in i or 'https://bit.ly/pdisk_tuts' in i or 'telegra.ph' in i):
             new_List.remove(i)
     return new_List
 
