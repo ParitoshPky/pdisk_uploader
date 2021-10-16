c6dd93e41475fe13ce2f0ff1929b07319072e81e
Paritosh Kumar <33643362+ParitoshPky@users.noreply.github.com>
2021-10-16 12:10:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -132,7 +132,7 @@ async def multi_pdisk_up(ml_string):
 async def new_pdisk_url(urls):
     new_urls = []
     for i in urls:
-        time.sleep(0.2)
+        time.sleep(0.3)
         new_urls.append(await pdisk_up(i))
     return new_urls
 
